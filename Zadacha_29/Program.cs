07bc645b1f908a55b58b34512f5b30197530139e
Igor Dudin <dudinio@mail.ru>
2023-08-12 13:02:54 +0300

    Решена задача 29

---
+++ Zadacha_29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int [] Array={6,1,33};
+
+Console.Write("[");
+for (int i=0;i<Array.Length-1;i++)
+{
+    Console.Write($"{Array[i]}, ");
+}
+int index=Array.Length;
+
+Console.Write($"{Array[index-1]}]");
